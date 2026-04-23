9a1e986ac0cf177de85a43b5ca461a45fc26d225
sinchana-src <sinchusinchana291@gmail.com>
2026-04-23 17:45:07 +0530

    Add files via upload

---
+++ objectcreation.java
@@ -0,0 +1,15 @@
+package day6;
+
+class student {
+	int age= 19;
+	String name="sinchana";
+}
+
+public class objectcreation {
+
+	public static void main(String[] args) {
+		student s = new student();
+		System.out.println(s.age);
+		System.out.println(s.name);
+		}
+}
--- pattern.java
+++ pattern.java
@@ -1,13 +1,16 @@
-package day4;
+package day5;
 
 public class pattern {
-	
-    public static void main(String[] args) {
-        for (int i = 1; i <= 5; i += 2) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
-            }
-            System.out.println(); // move to next line
-        }
-    }
+
+	public static void main(String[] args) {
+		for(int i=1; i<=5; i++)
+	       {
+	    	   for(int j=1; j<=5; j++)
+	    	   {
+	    		   System.out.print("*");
+	    	   }
+	    	   System.out.println();
+	       }
+	       
+	}	
 }
